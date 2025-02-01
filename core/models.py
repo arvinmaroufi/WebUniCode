4a3ef1beb28ba25361c4121e9aff21db076da5bc
Ashkan <ashkanghodrati01@gmail.com>
2025-02-01 20:57:50 +0330

    small change

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    completed_projects = models.IntegerField(default=0, verbose_name='پروژه های انجام شده')
+    satisfied_customers = models.IntegerField(default=0, verbose_name='مشتریان راضی')
+    members = models.IntegerField(default=0, verbose_name='تعداد اعضای تیم')
+    full_support = models.IntegerField(default=0, verbose_name='پشتیبانی')
+
+    class Meta:
+        verbose_name = 'مجموعه'
+        verbose_name_plural = 'مجموعه ها'
+
+    def __str__(self):
+        return str('شرایط مجموعه')
--- core/views.py
+++ core/views.py
@@ -1,16 +1,23 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.views import View
+from .models import Team
 from customer.models import Comment, Newsletter
 
 
 class HomeView(View):
     def get(self, request):
         comment = Comment.objects.all()
-        return render(request, 'core/home.html', {'comment': comment})
+        team = Team.objects.last()
+        return render(request, 'core/home.html', {'comment': comment, 'team': team})
 
     def post(self, request):
         email = request.POST.get('email')
-        Newsletter.objects.create(email=email)
+        if Newsletter.objects.filter(email=email).exists():
+            messages.error(request, 'شما قبلا عضو خبرنامه ما شده اید.')
+        else:
+            Newsletter.objects.create(email=email)
+            messages.success(request, 'ایمیل شما با موفقیت ثبت شد.')
         return render(request, 'core/home.html')
 
 
