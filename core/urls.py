58cc152946c83335954e6c64fca147740e6473d9
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-02-06 05:39:03 +0330

    Add Load More Button

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 app_name = 'core'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('faq', views.FaqView.as_view(), name='faq'),
-    path('contact', views.ContactView.as_view(), name='contact'),
+    path('faq/', views.FaqView.as_view(), name='faq'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
 ]
