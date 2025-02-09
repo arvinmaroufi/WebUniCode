62d646475677e33a5d71802edadae627f06b3fc9
Ashkan <ashkanghodrati01@gmail.com>
2025-02-09 15:07:58 +0330

    Auto stash before merge of "master" and "origin/master"

--- project/views.py
+++ project/views.py
@@ -1,8 +1,14 @@
 from .models import Project
 from django.views.generic import DetailView
+from core.models import SiteSettings
 
 
 class ProjectView(DetailView):
     template_name = 'project/project.html'
     model = Project
     context_object_name = 'project'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['site_settings'] = SiteSettings.objects.first()
+        return context
